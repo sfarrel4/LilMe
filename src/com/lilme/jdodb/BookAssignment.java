938b40093a17b15e9280d94e4a00fedfd2d11a08
jeremygalvan <jeremygalvan@10.159.168.254>
2015-11-09 17:10:28 -0500

    created object BookAssignment.java

--- src/com/lilme/jdodb/BookAssignment.java
+++ src/com/lilme/jdodb/BookAssignment.java
@@ -1,7 +1,7 @@
 package com.lilme.jdodb;
 
 import java.util.List;
-
+import java.util.Date;
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 import javax.jdo.annotations.IdGeneratorStrategy;
@@ -9,7 +9,6 @@ import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.Persistent;
 import javax.jdo.annotations.PrimaryKey;
 
-import com.google.appengine.api.datastore.Blob;
 
 @PersistenceCapable
 public class BookAssignment {
@@ -19,95 +18,62 @@ public class BookAssignment {
 	    private Long id;
 	    
 	    @Persistent
-	    private String bookTitle;
+	    private Long childID;
 	    
 	    @Persistent
-	    private String bookAuthor;
+	    private Long bookID;
 	    
 	    @Persistent
-	    private String bookSynop;
+	    private Date  readDate;
+	  
 	    
-	    @Persistent
-	    private String bookLessons;
-	    
-	    @Persistent
-	    private String bookQuestions;
-	    
-	    @Persistent
-	    private String bookImageURL;
-	    
-	    public BookAssignment(String bookTitle, String bookAuthor, String bookSynop, String bookLessons, String bookQuestions, String bookImageURL){
-	    	this.bookTitle = bookTitle;
-	    	this.bookAuthor = bookAuthor;
-	    	this.bookSynop = bookSynop;
-	    	this.bookLessons = bookLessons;
-	    	this.bookQuestions = bookQuestions;
-	    	this.bookImageURL = bookImageURL;
+	    public BookAssignment(Long childID, Long bookID){
+	    	this.childID = childID;
+	    	this.bookID = bookID;
+	    	this.readDate =  new Date();
+
 	    }
 	    
-	    public Long getId() {
+	    public Long getID() {
 	        return id;
 	    }
 	    
-	    public String getBookTitle(){
-	    	return bookTitle;
+	    public Long getChildId(){
+	    	return childID;
 	    }
 	    
-	    public void setBookTitle(String bookTitle){
-	    	this.bookTitle = bookTitle;
+	    public Long getBookID(){
+	    	return bookID;
+	    }
+	    public Date getReadDate(){
+	    	return readDate;
 	    }
 	    
-	    public String getBookAuthor(){
-	    	return bookAuthor;
+	    public void setID(Long id){
+	    	this.id= id;
 	    }
 	    
-	    public void setBookAuthor(String bookAuthor){
-	    	this.bookAuthor = bookAuthor;
+	    public void setChildID(Long childID){
+	    	this.childID = childID   ;
 	    }
 	    
-	    public String getBookSynop(){
-	    	return bookSynop;
+	    public void setBookID(Long bookID){
+	    	this.bookID = bookID   ;
 	    }
 	    
-	    public void setBookSynop(String bookSynop){
-	    	this.bookSynop = bookSynop;
-	    }
 	    
-	    public String getBookLessons(){
-	    	return bookLessons;
-	    }
-	    
-	    public void setBookLessons(String bookLessons){
-	    	this.bookLessons = bookLessons;
-	    }
-	    
-	    public String getBookQuestions(){
-	    	return bookQuestions;
-	    }
-	    
-	    public void setBookQuestions(String bookQuestions){
-	    	this.bookQuestions = bookQuestions;
-	    }
-	    
-	    public String getBookImage(){
-	    	return bookImageURL;
-	    }
-	    
-	    public void setBookImage(String bookImageURL){
-	    	this.bookImageURL = bookImageURL;
-	    }
-	    
+
 	    @SuppressWarnings("unchecked")
-		public static List<BookObject> getAllBooks(){
+		public static List<BookAssignment> getAllBooks(){
 	    	PersistenceManager pm = PMF.get().getPersistenceManager();
-			List<BookObject> results = null;
+			List<BookAssignment> results = null;
 			try {
-				Query q = pm.newQuery(BookObject.class);
-				q.setOrdering("bookTitle asc");
-				results = (List<BookObject>) q.execute();
+				Query q = pm.newQuery(BookAssignment.class);
+				q.setOrdering("readDate asc");
+				results = (List<BookAssignment>) q.execute();
 			} catch (Exception e) {
 				e.printStackTrace();
-				System.out.println("Books not Found");
+				System.out.println("Book Assignment not Found");
 			}
 			return results;
 	    }

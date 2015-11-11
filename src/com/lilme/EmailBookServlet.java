0191f19cde4c78e946a82f67861bf8834b76cf05
Stephen Farrell <sfarrel4@gmu.edu>
2015-11-11 16:28:56 -0500

    single email functionality

--- src/com/lilme/EmailBookServlet.java
+++ src/com/lilme/EmailBookServlet.java
@@ -1,6 +1,10 @@
 package com.lilme;
 
 import java.io.IOException;
+import javax.activation.DataHandler;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 import javax.jdo.PersistenceManager;
@@ -33,6 +37,13 @@ public class EmailBookServlet extends HttpServlet {
 		
 		String bookTitle = null;
 		String bookImageUrl = null;
+		String bookSum = null;
+		String bookAuthor = null;
+		String bookLessons = null;
+		String bookQuestions = null;
+		String childName = "Stephen Farrell";
+		String dateRead = "11/11/2015";
+		
 	
 		
 		PersistenceManager pm = PMF.get().getPersistenceManager();
@@ -41,6 +52,11 @@ public class EmailBookServlet extends HttpServlet {
 			BookObject b = pm.getObjectById(BookObject.class, bookID);
 			bookTitle = b.getBookTitle();
 			bookImageUrl = b.getBookImage();
+			bookSum = b.getBookSynop();
+			bookAuthor = b.getBookAuthor();
+			bookLessons = b.getBookLessons();
+			bookQuestions = b.getBookQuestions();
+			
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -49,15 +65,48 @@ public class EmailBookServlet extends HttpServlet {
 		Properties props = new Properties();
 		Session session = Session.getDefaultInstance(props, null);
 		
-		String msgBody = "Your Child read " + bookTitle + "\n Here is a link to an image of the book: \n" + bookImageUrl  ;
+			String htmlBody = "<!DOCTYPE html>"
+					+ "<html>"
+					+ "<head>"
+					+ "<title> </title>"
+					+ "</head>"
+					+ "<body>"
+						+ "<h1>Reading History for your child:  "+ childName + "</h1>"
+						+ "<p>Date of reading: "+ dateRead + "</p>"
+						+ "<h2>Today's Book was</h2>	<br><br>"
+						+ "<br>	<img src="+ bookImageUrl + " />"
+						+ "<br><br><br><br>"
+						+ " <br>"
+						+ "<b>Title</b>: <u>"+ bookTitle + "</u><br><br>"
+						+ "<b>Author</b>:" + bookAuthor + " <br><br>"
+						+ "<B>Summary:</b> <br>"
+						+ ""+ bookSum +" "
+						+ "<b>Lessons Learned:</b><br>"
+						+ "" + bookLessons + ""
+						+ "<br><br>"
+						+ "<b>Continue the Learning!:<br> </b>"
+						+ "" + bookQuestions + ""
+						+ "<br><br>"
+						+ "</body>"
+						+ "</html>";        
+	       
+
+	        Multipart mp = new MimeMultipart();
+
+	        MimeBodyPart htmlPart = new MimeBodyPart();
+	       
+		
+		//String msgBody = "Your Child read " + bookTitle + "\n Here is a link to an image of the book: \n" + bookImageUrl  ;
 		
 		try{
 			Message msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress("ait390s1503josh@gmail.com", "Lil' Me Daycare"));
+			msg.setFrom(new InternetAddress("ait390s1503stephen@gmail.com", "Lil' Me Daycare"));
 			msg.addRecipient(Message.RecipientType.TO, 
 					new InternetAddress(email, "Parent"));
 			msg.setSubject("Take a look at what your child read today");
-			msg.setText(msgBody);
+			htmlPart.setContent(htmlBody, "text/html");
+		    mp.addBodyPart(htmlPart);
+		    msg.setContent(mp);
 			Transport.send(msg);
 			
 		}
